4773875c7ee994445fd7e3380e1ca88c8306bb58
Hamza Boussetta <hbousset@c3r1p10.1337.ma>
2025-01-20 09:17:40 +0100

    handle esc and close window , no leaks

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: hbousset < hbousset@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 11:26:58 by hbousset          #+#    #+#             */
-/*   Updated: 2025/01/20 09:10:22 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/01/20 09:17:15 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,11 @@ typedef struct s_game
 	t_txr	textures;
 }	t_game;
 
-typedef struct s_cleanup
-{
-	void	*mlx;
-	void	*window;
-	char	**map;
-}	t_cleanup;
-
 //map
 char	*read_map(const char *map);
 int		check_map(char *map);
 void	init_map(void *mlx, void *window, char **map);
 void	render_map(void *mlx, void *window, char **map, t_txr *textures);
 void	free_split(char **lines);
-
 
 #endif
